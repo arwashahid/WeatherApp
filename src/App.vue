6ac81536e9adfac528861cb4f900921af267a6ed
arwa052 <arwashahid052@gmail.com>
2022-01-02 16:36:06 +0500

    day#01 Weather App

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,137 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" :class="typeof weather.main != 'undefined' && weather.main.temp > 16 ? 'warm' : ''">
+    <main>
+      <div class="search-box">
+        <input 
+          type="text" 
+          class="search-bar" 
+          placeholder="Search..."
+          v-model="query"
+          @keypress="fetchWeather"
+        />
+      </div>
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined' " >
+        <div class="location-box">
+          <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
+          <div class="date">{{ dateBuilder() }}</div>
+        </div>
+        <div class="weather-box">
+          <div class="temp">{{ Math.round(weather.main.temp) }}°c</div>
+          <div class="weather">{{ weather.weather[0].main }}</div>
+        </div>
+      </div>
+      
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  import axios from 'axios'
+  import moment from 'moment'
+  export default {
+    name: 'app',
+    data () {
+      return {
+        api_key: '66176be97d4eaf156d84f0689346b7db',
+        url_base: 'https://api.openweathermap.org/data/2.5/',
+        query: '',
+        weather: {}
+      }
+    },
+    methods: {
+      fetchWeather (e) {
+        if (e.key == "Enter") {
+          axios.get( this.url_base + 'weather?q=' + this.query + '&units=metric&APPID=' + this.api_key)
+          .then(res => {
+            this.weather = res.data;
+          })
+        }
+      },
+      dateBuilder () {
+        return moment().format('dddd MMMM Do YYYY');
+      }
+    }
   }
-}
 </script>
 
-<style>
+<style scoped>
+*{
+    margin: 0;
+    padding: 0;
+    font-family: 'Montserrat', sans-serif;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    background-image: url('./assets/cold-bg.jpg');
+    background-size: cover;
+    background-position: bottom;
+    transition: 0.4s;
+}
+#app.warm {
+    background-image: url('./assets/warm-bg.jpg');
+}
+main {
+    min-height: calc(100vh - 50px);
+    padding: 25px;
+    background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.75));
+}
+.search-box {
+    width: 100%;
+    margin-bottom: 30px;
+}
+.search-box .search-bar {
+    display: block;
+    width: calc(100% - 30px);
+    padding: 15px;
+    color: #313131;
+    font-size: 20px;
+    appearance: none;
+    border:none;
+    outline: none;
+    background: none;
+    box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
+    background-color: rgba(255, 255, 255, 0.5);
+    border-radius: 0px 16px 0px 16px;
+    transition: 0.4s;
+}
+.search-box .search-bar:focus {
+    box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+    background-color: rgba(255, 255, 255, 0.75);
+    border-radius: 16px 0px 16px 0px;
+}
+.location-box .location {
+    color: #FFF;
+    font-size: 32px;
+    font-weight: 500;
+    text-align: center;
+    text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+}
+.location-box .date {
+    color: #FFF;
+    font-size: 20px;
+    font-weight: 300;
+    font-style: italic;
+    text-align: center;
+}
+.weather-box {
+    text-align: center;
+}
+.weather-box .temp {
+    display: inline-block;
+    padding: 10px 25px;
+    color: #FFF;
+    font-size: 102px;
+    font-weight: 900;
+    text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+    background-color:rgba(255, 255, 255, 0.25);
+    border-radius: 16px;
+    margin: 30px 0px;
+    box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+.weather-box .weather {
+    color: #FFF;
+    font-size: 48px;
+    font-weight: 700;
+    font-style: italic;
+    text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
 }
 </style>
